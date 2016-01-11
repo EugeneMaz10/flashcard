1cde802dfcb1bcce20f69785a46efba085734b1d
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-11 13:13:22 -0800

    Add definitions to terms

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,55 @@ $(document).ready(function() {
   $(".button1").click(function(){
     $(".show1").toggle();
   });
+  $(".button2").click(function(){
+    $(".show2").toggle();
+  });
+  $(".button3").click(function(){
+    $(".show3").toggle();
+  });
+  $(".button4").click(function(){
+    $(".show4").toggle();
+  });
+  $(".button5").click(function(){
+    $(".show5").toggle();
+  });
+  $(".button6").click(function(){
+    $(".show6").toggle();
+  });
+  $(".button7").click(function(){
+    $(".show7").toggle();
+  });
+  $(".button8").click(function(){
+    $(".show8").toggle();
+  });
+  $(".button9").click(function(){
+    $(".show9").toggle();
+  });
+  $(".button10").click(function(){
+    $(".show10").toggle();
+  });
+  $(".button11").click(function(){
+    $(".show11").toggle();
+  });
+  $(".button12").click(function(){
+    $(".show12").toggle();
+  });
+  $(".button13").click(function(){
+    $(".show13").toggle();
+  });
+  $(".button14").click(function(){
+    $(".show14").toggle();
+  });
+  $(".button15").click(function(){
+    $(".show15").toggle();
+  });
+  $(".button16").click(function(){
+    $(".show16").toggle();
+  });
+  $(".button17").click(function(){
+    $(".show17").toggle();
+  });
+  $(".button18").click(function(){
+    $(".show18").toggle();
+  });
 });
