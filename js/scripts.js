4e31ebd2ba0e7b0fd1a0e5a2888fa8a3c5837340
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-11 11:42:55 -0800

    Setting up html flash cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
-  $(".clickable1").click(function(){
-    $(".section2").toggle();
-  });
-  $("#clickable1").click(function(){
-    $(".showp").toggle();
-  });
-  $(".clickable2").click(function(){
-    $(".section3").fadeIn();
-  });
-  $(".clickable2b").click(function(){
-    $(".section3").fadeOut();
-  });
-  $(".clickable4").click(function(){
-    $(".section4").toggle();
-  });
-  $("#clickable4").click(function(){
-    $(".showp4").toggle();
+  $(".button1").click(function(){
+    $(".show1").toggle();
   });
 });
